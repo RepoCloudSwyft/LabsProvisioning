33a2001f6ba76111af3a7f3d67ebedfc56ae94f6
johnpatrick23 <johnpatrickpapares@gmail.com>
2022-04-12 11:01:24 +0800

    Add model

---
+++ LabsProvisioning/Model/LabsProvisionModel.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Azure.Management.Compute.Fluent.Models;
+
+namespace LabsProvisioning.Model
+{
+    public class LabsProvisionModel
+    {
+        public string Location { get; set; }
+
+        public string Environment { get; set; }
+
+        public string ClientCode { get; set; }
+
+        public string VirtualMachineName { get; set; }
+
+        public string OsType { get; set; }
+
+        public string Size { get; set; } = VirtualMachineSizeTypes.StandardB2ms.Value;
+
+        public bool IsCustomTemplate { get; set; } = false;
+
+        public int TempStorageSizeInGb { get; set; } = 127;
+
+        public string ImageUri { get; set; }
+
+        public string ContactPerson { get; set; }
+
+        public string StorageAccountName { get; set; }
+
+        public string ComputerName { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string SubscriptionId { get; set; }
+
+        public string ApplicationId { get; set; }
+
+        public string ApplicationKey { get; set; }
+
+        public string TenantId { get; set; }
+
+        public bool DeallocateWhenFinish { get; set; }
+
+    }
+}
---
+++ LabsProvisioning/Model/TemplateParameterVmModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabsProvisioning.Model
+{
+    public class TemplateParameterVmModel
+    {
+        public string networkSecurityGroupId { get; set; }
+
+        public string subnetName { get; set; } = "virtual-machines-labs-subnet";
+
+        public string virtualNetworkId { get; set; }
+
+        public string virtualMachineName { get; set; }
+
+        public string computerName { get; set; }
+
+        public string adminUsername { get; set; }
+
+        public string adminPassword { get; set; }
+        
+        public string storageAccountName { get; set; }
+        
+        public string imageUri { get; set; }
+        
+        public object tags { get; set; }
+        
+        public string virtualMachineSize { get; set; }
+        
+        public int diskSizeGB { get; set; }
+        
+        public string newTemplateName { get; set; }
+        
+        public string publicIpAddressType { get; set; }
+        
+        public string publicIpAddressSku { get; set; }
+    }
+}
