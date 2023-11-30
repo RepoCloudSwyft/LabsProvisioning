2f8382f8c63b4f1bd2b2deef07cf43ede2afc154
johnpatrick23 <johnpatrickpapares@gmail.com>
2023-12-01 01:14:28 +0800

    v2.2.1

--- LabsProvisioning/Properties/Resources.Designer.cs
+++ LabsProvisioning/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace LabsProvisioning.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -116,6 +116,16 @@ namespace LabsProvisioning.Properties {
         internal static byte[] azuredeploywindowsmanaged {
             get {
                 object obj = ResourceManager.GetObject("azuredeploywindowsmanaged", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] azuredeploywindowsmanagedgallery {
+            get {
+                object obj = ResourceManager.GetObject("azuredeploywindowsmanagedgallery", resourceCulture);
                 return ((byte[])(obj));
             }
         }
